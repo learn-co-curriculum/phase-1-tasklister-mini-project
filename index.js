a3d367712ebdfc7a9e2871e57586ca686cf19de4
github username <andrewcohn0@gmail.com>
2017-12-15 13:52:47 -0500

    basic solution implemented

---
+++ index.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //grab all the necessary DOM elements
+
+  //form and relevant input fields
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  //ul where new tasks will live on the DOM
+  const newTaskUl = document.getElementById("tasks");
+
+  //attach event listeners
+  newTaskForm.addEventListener("submit", () =>
+    createNewTask(event, newTaskDescription)
+  );
+});
+
+const createNewTask = (event, newTaskDescription) => {
+  //stop form from trying to submit
+  event.preventDefault();
+
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
