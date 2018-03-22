bd3a45176de65550d31be12c7729077d49a96ca0
Andrew Cohn <andrewcohn0@gmail.com>
2018-03-22 14:18:19 -0400

    changes to starter code; now even with oo solution

--- src/task.js
+++ src/task.js
@@ -1,14 +1,2 @@
 class Task {
-  constructor(description) {
-    this.description = description;
-  }
-
-  render() {
-    return `
-      <li>
-        ${this.description}
-        <button data-description="${this.description}">X</button>
-      </li>
-      `;
-  }
 }
