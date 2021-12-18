796ca2195faa501fb849123337054a5fbc05e349
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-18 11:49:52 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.newtaskdescription.value);
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let li = document.createElement('li');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo}  `
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
