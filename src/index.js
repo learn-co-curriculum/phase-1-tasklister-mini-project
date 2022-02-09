0784fdba3a9e483e855afbfae0dd274aed3202af
Kristina Peru <krissy.germanov@gmail.com>
2022-02-08 17:52:49 -0700

    Everything
    Co-authored-by: Olivia Pomeroy <opomeroy26@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.querySelector('form');
+  //console.log(form);
+  form.addEventListener('submit', function(event){
+  event.preventDefault();
+  let description = document.getElementById("new-task-description").value;
+  
+  // let ul = document.getElementById("tasks");
+  // console.log(ul);
+  // ul.appendChild(description);
+  //let div = document.createElement("div")
+  let div = document.getElementById("list");
+  let p = document.createElement("p")
+  div.append(p)
+  p.innerText = description
+  //console.log(div);
+  
+})
+
 });
+
