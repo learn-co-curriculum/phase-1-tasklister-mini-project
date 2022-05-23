00c7eb937059bb1a2349120f88cb00a1a78822a3
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-23 15:51:21 -0600

    first deliverables done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+  document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newTask = e.target[ 'new-task-description' ].value;
+    const li = document.createElement('li')
+    li.textContent = newTask
+    document.getElementById('tasks').appendChild(li)
+  })
 });
