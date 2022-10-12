34350b84a71bb9c967bc20b8a64743a8e03ecda4
gehrig <gehrigbarnes@gmail.com>
2022-10-12 10:39:37 -0500

    built out the buildToDoFunction. Again, when a user clicks submit, they will invoke the buildToDo funciton and pass in the targeted input value. When this funciton is invoked, it first creates an li element and a button element. After, the elements are created, it then sets the text content of the button element to 'x' and the li text content to the toDo params. After this, we then append the button to the li element; and subsequently append the new li element to the ul element with the id of 'tasks'. After this, we attached a click event listener to our button that calls the handleDelete function. Next, I will code out the handleDelete function and provide more detail in my next commit.

--- src/index.js
+++ src/index.js
@@ -17,3 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
     form.reset()
   })
 });
+
+  //when user submits, we pass in the targeted input into buildToDo function
+  function buildToDo(toDo){
+    //first, the buildToDo function creates an li element and a button element. 
+    let li = document.createElement('li');
+    let btn = document.createElement('button')
+
+    //next, we set the textContent of the btn element to 'x'.
+    btn.textContent = 'x'
+
+    //after, we set the textContent of the li element to the todo parameter.
+    //remember, when we submit, we are passing in targeted input into the parameters. 
+    li.textContent = `${toDo}`
+
+    //next, we set the text content for the li and button element, we then append the new btn to the li.
+    li.appendChild(btn);
+
+    //then, we added the li element to the ul element with id of tasks
+    document.querySelector('#tasks').appendChild(li);
+  
+  
+    //finally, we attached a click event listener to the btn which invokes the handleDelete function
+    btn.addEventListener('click', handleDelete)
+  }
