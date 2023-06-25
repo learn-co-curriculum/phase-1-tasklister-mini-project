0a5c5f78e44c785f8161b0d766f71f9f44336bc9
Sean <ohsean812@gmail.com>
2023-06-25 18:57:30 -0400

    commit tree bd1d7d5b07f5332dc1be1d9a02fdd1e9f91d2d0f

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  });
+  
+  const form = document.querySelector("form")
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const tasks = document.getElementById("tasks")
+
+    const task = document.createElement("li");
+    task.innerText = document.getElementById("new-task-description").value + " "
+    tasks.append(task)
+
+    const button = document.createElement("button")
+    button.innerText = "X"
+    task.append(button)
+
+    button.addEventListener("click", function() {
+      task.remove()
+    })
+
+
+    form.reset()
+  })
+});
+
