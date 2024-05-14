a4be9fcdce43b09a00ba8cae99d8053af6633b4f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-14 10:57:53 -0400

    Add create/delete functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+const form = document.querySelector('#create-task-form')
+const submitButton = document.querySelector("#create-task-form input[type='Submit']")
+console.log(submitButton)
+
+
+const createNewTask = (e) => {
+  e.preventDefault()
+  const taskText = e.target.previousElementSibling.value
+  if(taskText !== ''){
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+    btn.textContent = 'X'
+    btn.addEventListener('click', handleDeleteTask)
+    li.textContent = `${taskText} `
+    li.appendChild(btn)
+    document.querySelector("#tasks").appendChild(li)
+    form.reset()
+  }
+}
+
+const handleDeleteTask = (e) => {
+  e.target.parentNode.remove()
+}
+
+submitButton.addEventListener('click', createNewTask)
+
+
+
