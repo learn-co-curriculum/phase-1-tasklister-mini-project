0621ca7155137e0039ae850c8e4beaf0e5920fe0
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-15 11:46:47 -0400

    Add ability to add and remobe items from todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    handleToDo();
+
+})
+})
+
+function handleToDo() {
+  let searchBoxValue = document.getElementById("new-task-description").value 
+  let listItem = document.createElement('li')
+  let deleteBtn = document.createElement('button')
+  listItem.textContent = searchBoxValue
+  deleteBtn.textContent = ' X '
+  document.getElementById("tasks").appendChild(listItem).appendChild(deleteBtn)
+
+  deleteBtn.addEventListener('click', () => listItem.remove())
+}
+
