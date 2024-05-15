67b85372917391c00eb8d1b05552bda16b686db3
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-14 22:47:53 -0400

    Create lister that add item to list with submit button.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    logInput(e.target.querySelector('#new-task-description').value)
+  })
 });
+/// tried with getElementbyId and got error could it be the dashes.
+/// used the element and hovered over input and confirmed #new-task-description
+
+function logInput(task) {
+  let listTask = document.createElement('li')
+  listTask.textContent = task
+  document.querySelector("#list").appendChild(listTask)
+}
+
+
