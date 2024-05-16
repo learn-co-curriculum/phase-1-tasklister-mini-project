7b1ba6dcf3899a97182474faeb7fc65c4702a14b
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-16 15:01:21 -0400

    Move table-row creation into seperate function

--- src/index.js
+++ src/index.js
@@ -6,31 +6,35 @@ const createNewTask = (e) => {
   const taskText = e.target['new-task-description'].value
   const taskPriority = e.target['new-task-priority'].value
   if(taskText !== '' && taskPriority !== ''){
-    handleTaskPOST(taskText, taskPriority).then(data => {
-      const tr = document.createElement('tr')
-      tr.id = data.id
-      const td1 = document.createElement('td')
-      const td2 = document.createElement('td')
-      const td3 = document.createElement('td')
+    handleTaskPOST(taskText, taskPriority)
+    .then(data => {
       const editBtn = createEditButton(data.id);
       const deleteBtn = createDeleteButton(data.id)
-      tr.className = taskPriority
-      td1.textContent = taskText
-      td2.appendChild(deleteBtn)
-      td3.appendChild(editBtn)
-      tr.appendChild(td1)
-      tr.appendChild(td2)
-      tr.appendChild(td3)
+      tr = createRow(data.id, taskText, taskPriority, deleteBtn, editBtn)
       document.querySelector('#task-table').appendChild(tr)
       document.querySelector("#new-task-description").value = ''
       document.querySelector("#task-priority").value = ''
-      //form.reset()
     })
     .catch(error => {
       console.log(error)
     })
-    
   }
+}
+
+const createRow = (id, taskText, taskPriority, deleteBtn, editBtn) => {
+  const tr = document.createElement('tr')
+  tr.id = id
+  tr.className = taskPriority
+  const td1 = document.createElement('td')
+  td1.textContent = taskText
+  const td2 = document.createElement('td')
+  td2.appendChild(deleteBtn)
+  const td3 = document.createElement('td')
+  td3.appendChild(editBtn)
+  tr.appendChild(td1)
+  tr.appendChild(td2)
+  tr.appendChild(td3)
+  return tr
 }
 
 //Create edit button and return
@@ -107,7 +111,6 @@ const handleTaskPATCH = (e) => {
   })
   .then(res => res.json())
   .then(data => {
-    //initApp();
     document.querySelector("#new-task-description").value = ''
     document.querySelector("#task-priority").value = ''
     submitButton.className = 'btn'
@@ -118,11 +121,11 @@ const handleTaskPATCH = (e) => {
     document.querySelectorAll('tr').forEach(tr => {
       tr.remove()
     })
-    initApp()
+    initApp();
   })
   .catch(error => {
     alert(`Something went wrong while trying to update task: ${taskText}`)
-    console.log(error)
+  console.log(error)
   })
 }
 
@@ -130,7 +133,6 @@ const editTask = (e) => {
   const task = e.target.closest('tr').firstChild.textContent
   const priority = e.target.closest('tr').className
   const id = e.target.attributes['task-id'].value
-  // console.log(id)
   document.querySelector("#new-task-description").value = task
   document.querySelector("#task-priority").value = priority
   submitButton.className = 'editBtn'
@@ -145,20 +147,9 @@ const renderTasks = (body) => {
   document.querySelector("#task-priority").value = ''
   document.querySelectorAll('tr').forEach(tr => console.log(tr))
   body.forEach(item => {
-    const tr = document.createElement('tr')
-    const td1 = document.createElement('td')
-    const td2 = document.createElement('td')
-    const td3 = document.createElement('td')
     const editBtn = createEditButton(item.id)
     const deleteBtn = createDeleteButton(item.id)
-    tr.id = item.id
-    tr.className = item.priority
-    td1.textContent = item.task
-    td2.appendChild(deleteBtn)
-    td3.appendChild(editBtn)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    tr.appendChild(td3)
+    tr = createRow(item.id, item.task, item.priority, deleteBtn, editBtn)
     document.querySelector('#task-table').appendChild(tr)
   })
 }
